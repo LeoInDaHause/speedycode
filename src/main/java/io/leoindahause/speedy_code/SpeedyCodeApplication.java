6aa43a211d88b7767b51293e7f545eb3831c7dba
Cesar Leonardo Rodriguez Leon <138258855+LeoInDaHause@users.noreply.github.com>
2024-07-26 15:41:39 -0500

    Add files via upload

---
+++ src/main/java/io/leoindahause/speedy_code/SpeedyCodeApplication.java
@@ -0,0 +1,13 @@
+package io.leoindahause.speedy_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedyCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedyCodeApplication.class, args);
+	}
+
+}
