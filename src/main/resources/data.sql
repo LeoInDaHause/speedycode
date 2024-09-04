2cab11b27b5546e676a6f141b5aa5792e4297a29
Cesar Leonardo Rodriguez Leon <cesleo0121@gmail.com>
2024-09-04 17:01:59 -0500

    git push

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,5 @@
-INSERT INTO person (id, email, password, exercise_status_id) VALUES (0, NULL, NULL, NULL);
+MERGE INTO person AS p
+USING (SELECT 0 AS id) AS src
+ON (p.id = src.id)
+WHEN NOT MATCHED THEN
+INSERT (id, email, password, exercise_status_id) VALUES (0, NULL, NULL, NULL);
